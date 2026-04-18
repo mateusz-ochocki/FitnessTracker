e5b812c0aa7ff6f2295358e41d29436feee0bbad
vdi-terminal <mateusz.leon.ochocki@gmail.com>
2026-04-18 10:18:47 +0200

    LABORATORIUM_3

--- src/main/java/pl/wsb/fitnesstracker/event/Event.java
+++ src/main/java/pl/wsb/fitnesstracker/event/Event.java
@@ -1,5 +1,53 @@
 package pl.wsb.fitnesstracker.event;
 
-// TODO: Define the Event entity with appropriate fields and annotations
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
 public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private LocalDate startDate;
+
+    private String location;
+
+    public Event() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
